50bf300a5019c20e4e950829bcc7c6398093f371
ductran689 <td.duc89@gmaill.com>
2023-09-05 21:48:35 +1000

    fix bug 3

--- pages/api/carousel.js
+++
@@ -1,7 +0,0 @@
-import { getCarousel } from '../../prisma/admin';
-
-export default async function handle(req, res) {
-  const pics = await getCarousel(req.query.id);
-
-  return res.status(200).json(pics);
-}
--- pages/api/services.js
+++
@@ -1,7 +0,0 @@
-import { getAllPics } from '../../prisma/admin';
-
-export default async function handle(req, res) {
-  const pics = await getAllPics(req.query.id);
-  console.log('getuser');
-  return res.status(200).json(pics);
-}
